4d5e5cdfd4a2c923186a7f7efc53c498308d04d4
Ryan Govostes <rgovostes+git@gmail.com>
2025-02-27 16:06:53 -0800

    Add expected output for comparison

--- test.sh
+++ test.sh
@@ -7,4 +7,17 @@ text2pcap -F pcap -D -u 14641,14641 inputs.txt inputs.pcap
 # Use tshark to dissect the packets using our dissector
 tshark -X lua_script:pascal.lua -V -O pascal -r inputs.pcap > dissected.txt
 
-cat dissected.txt
+# The -v flag prints the dissected output
+if [[ "${1:-}" = "-v" ]]; then
+    cat dissected.txt
+    exit
+fi
+
+# Otherwise, diff with the expected output
+set +e
+diff expected.txt dissected.txt
+STATUS=$?
+if [ $STATUS -eq 0 ]; then
+    echo "Passed!"
+fi
+exit $STATUS
